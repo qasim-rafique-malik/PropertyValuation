27c3a0315dc40959674d7187a956a6b082cf7b35
Shahid Aslam <shahidaslam448@gmail.com>
2021-06-27 16:18:34 +0500

    rule model updated

--- Modules/Valuation/Entities/ValuationSowRule.php
+++ Modules/Valuation/Entities/ValuationSowRule.php
@@ -4,6 +4,11 @@ use Modules\Valuation\Entities\ValuationBaseModel;
 
 class ValuationSowRule extends ValuationBaseModel
 {
+    const ValuatorsLimitations='ValuatorsLimitations';
+    const InformationOfSources='InformationOfSources';
+    const TypeOfReport='TypeOfReport';
+    const RestrictionsOnDistribution='RestrictionsOnDistribution';
+    const ValuationReport='ValuationReport';
     public $timestamps = true;
     protected $fillable = array();
 

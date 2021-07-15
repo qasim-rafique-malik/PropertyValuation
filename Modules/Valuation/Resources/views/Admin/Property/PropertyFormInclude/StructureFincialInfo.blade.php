08030aa8565656836ee7713e13579c0d89395f32
Muneeb Ahmad <muneeb.ahmad@zecsn.com>
2021-07-15 21:26:19 +0500

    :bug: Fixed array error

--- Modules/Valuation/Resources/views/Admin/Property/PropertyFormInclude/StructureFincialInfo.blade.php
+++ Modules/Valuation/Resources/views/Admin/Property/PropertyFormInclude/StructureFincialInfo.blade.php
@@ -380,7 +380,7 @@
                                     <div class="col-md-4">
                                         <div class="form-group">
                                             <label class="control-label">Estimated value</label>
-                                            <input type="number" name="estimatedValueStructure" id="estimatedValueStructure" value="{{isset($estimatedValueStructure)?$estimatedValueStructure:0.00}}"
+                                            <input type="number" name="estimatedValueStructure" id="estimatedValueStructure" value="{{isset($estimatedValueStructure)?$estimatedValueStructure[0]:0.00}}"
                                                    class="form-control priceField"
                                                    autocomplete="nope">
                                         </div>
@@ -388,13 +388,13 @@
                                     <div class="col-md-4">
                                          <div class="form-group">
                                             <label class="control-label">Residual Value</label>
-                                            <input type="text" name="residual_value_for_Structure" class="form-control priceField" value="{{isset($residual_value_for_Structure)?$residual_value_for_Structure:''}}">
+                                            <input type="text" name="residual_value_for_Structure" class="form-control priceField" value="{{isset($residual_value_for_Structure)?$residual_value_for_Structure[0]:''}}">
                                          </div>
                                     </div>
                                      <div class="col-md-4">
                                          <div class="form-group">
                                             <label class="control-label">Depicted Value</label>
-                                            <input type="text" name="depicted_value_for_Structure" class="form-control priceField" value="{{isset($depicted_value_for_Structure)?$depicted_value_for_Structure:''}}">
+                                            <input type="text" name="depicted_value_for_Structure" class="form-control priceField" value="{{isset($depicted_value_for_Structure)?$depicted_value_for_Structure[0]:''}}">
                                          </div>
                                     </div>
                                 </div>
@@ -402,13 +402,13 @@
                                     <div class="col-md-4">
                                          <div class="form-group">
                                             <label class="control-label">Cost Of Construction</label>
-                                            <input type="text" name="cost_construction_for_Structure" class="form-control priceField" value="{{isset($cost_construction_for_Structure)?$cost_construction_for_Structure:''}}">
+                                            <input type="text" name="cost_construction_for_Structure" class="form-control priceField" value="{{isset($cost_construction_for_Structure)?$cost_construction_for_Structure[0]:''}}">
                                          </div>
                                     </div>
                                     <div class="col-md-4">
                                          <div class="form-group">
                                             <label class="control-label">Income Base Value</label>
-                                            <input type="text" name="incomebasevalue_for_Structure" class="form-control priceField" value="{{isset($incomebasevalue_for_Structure)?$incomebasevalue_for_Structure:''}}">
+                                            <input type="text" name="incomebasevalue_for_Structure" class="form-control priceField" value="{{isset($incomebasevalue_for_Structure)?$incomebasevalue_for_Structure[0]:''}}">
                                          </div>
                                     </div>
                                 </div>
